144eb980698ec56f73f5ae85d9693ae924692d2d
RMP Motos Dev <dev@rmpmotos.local>
2026-05-08 21:13:37 -0500

    Add Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import { StoreProvider } from './context/StoreContext'
 import './index.css'
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <StoreProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </StoreProvider>
   </React.StrictMode>
